a1b240375245763f633df8b43b4c7f314a190027
Yukaru <nilsrw@gmx.de>
2021-06-03 16:18:46 +0200

    fixed confirmation dialog loop

--- content.js
+++ content.js
@@ -3,48 +3,60 @@ var isFocused = true;
 var selfPaused = false;
 var ytVideo = $(".video-stream.html5-main-video")[0];
 
-// Focus event handlers 
-function onBlur() {
-	isFocused = false;
-};
-function onFocus(){
-	isFocused = true;
-};
-
-// Set the event handlers
-if (/*@cc_on!@*/false) { // check for Internet Explorer
-	document.onfocusin = onFocus;
-	document.onfocusout = onBlur;
-} else {
-	window.onfocus = onFocus;
-	window.onblur = onBlur;
+// Doesn't start script if there was an issue
+if (ytVideo !== undefined) {
+    initScript();
 }
 
-// Check if user paused manually
-$('.ytp-play-button.ytp-button, .video-stream.html5-main-video').on("click", function(){
-	selfPaused = !selfPaused;
-});
+// Focus event handlers 
+function onBlur() {
+    isFocused = false;
+};
 
-// Check for Youtube's Pause every half a second
-setInterval(function(){ 
+function onFocus() {
+    isFocused = true;
+};
 
-	// If Youtube is focussed and paused by the popup
-	if (isFocused) {
-		let continueDiv = $("div#main.yt-confirm-dialog-renderer");
-	
-		if (continueDiv.length > 0) {
-			let continueBtn = continueDiv.find("tp-yt-paper-button");
-		
-			if (continueBtn.length > 0) {
-				continueBtn.click();	
-			}
-		} 
-	}
-	
-	// If Youtube is not focussed and paused by the popup
-	if (!isFocused && ytVideo.paused && !selfPaused) {
-		ytVideo.play();
-	}
-	
-	
-}, 500);
+// Initializes the pause blocker
+function initScript() {
+
+    // Set the event handlers
+    if ( /*@cc_on!@*/ false) { // check for Internet Explorer
+        document.onfocusin = onFocus;
+        document.onfocusout = onBlur;
+    } else {
+        window.onfocus = onFocus;
+        window.onblur = onBlur;
+    }
+
+    // Check if user paused manually
+    $('.ytp-play-button.ytp-button, .video-stream.html5-main-video').on("click", function() {
+        selfPaused = !selfPaused;
+    });
+
+    // Check for Youtube's Pause every half a second
+    setInterval(function() {
+
+        // If Youtube is focussed and paused by the popup
+        if (isFocused) {
+            let continueDiv = $("div#main.yt-confirm-dialog-renderer");
+
+            if (continueDiv.length > 0) {
+                let continueBtn = continueDiv.find("tp-yt-paper-button");
+                let parentDiv = $(".style-scope.ytd-popup-container");
+
+                if (continueBtn.length > 0 && parentDiv.css('display') != 'none') {
+                    continueBtn.click();
+                }
+            }
+        }
+
+        // If Youtube is not focussed and paused by the popup
+        if (!isFocused && ytVideo.paused && !selfPaused) {
+            ytVideo.play();
+        }
+
+
+    }, 500);
+
+}
